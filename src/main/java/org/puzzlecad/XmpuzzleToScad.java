46ac0ab8c935782b4c3bce8dc21ae8f37cfc2bcd
Aaron Siegel <2014957+aaron-siegel@users.noreply.github.com>
2020-05-28 21:44:00 -0700

    Add licensing info to puzzlecad itself

--- src/main/java/org/puzzlecad/XmpuzzleToScad.java
+++ src/main/java/org/puzzlecad/XmpuzzleToScad.java
@@ -1,21 +1,41 @@
+/* ====================================================================
+
+  This is puzzlecad, an OpenSCAD library for modeling mechanical
+  puzzles. It is provided as part of the Printable Puzzle Project:
+  https://puzzlehub.org/ppp
+
+  To obtain the latest version of puzzlecad:
+  https://www.thingiverse.com/thing:3198014
+
+  Puzzlecad code repository:
+  https://github.com/aaron-siegel/puzzlecad
+
+  puzzlecad is (c) 2019-2020 Aaron Siegel and is distributed under
+  the MIT license. This means you may use or modify puzzlecad for any
+  purposes, including commercial purposes, provided that you include
+  the attribution "puzzlecad is (c) 2019-2020 Aaron Siegel" in any
+  distributions or derivatives of puzzlecad, along with a copy of
+  the MIT license.
+
+  For details of this license, please refer to the license-mit.txt
+  file distributed with puzzlecad, or visit:
+  https://opensource.org/licenses/MIT
+
+  NOTE THAT WHILE THE PUZZLECAD LIBRARY IS RELEASED UNDER THE MIT
+  LICENSE, INDIVIDUAL PUZZLE DESIGNS (INCLUDING VARIOUS DESIGNS THAT
+  ARE STORED IN THE PUZZLECAD GITHUB REPO) ARE SHARED UNDER A MORE
+  RESTRICTIVE LICENSE. You may not use copyrighted puzzle designs for
+  commercial purposes without explicit permission from the copyright
+  holder(s).
+
+==================================================================== */
+
 package org.puzzlecad;
 
-import static org.puzzlecad.XmpuzzleFile.*;
-
-import org.w3c.dom.Document;
-import org.w3c.dom.Node;
-import org.w3c.dom.NodeList;
-
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
 import java.io.File;
-import java.io.FileInputStream;
 import java.io.PrintWriter;
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
-import java.util.zip.GZIPInputStream;
+
+import static org.puzzlecad.XmpuzzleFile.GRID_TYPE_RECTILINEAR;
 
 public class XmpuzzleToScad {
 
